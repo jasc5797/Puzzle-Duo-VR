b71ebe42f82093acc02b020d0119de41932f4093
jasc5797 <jasc5797@yahoo.com>
2019-03-01 17:31:32 -0500

    Started working on my own maze generator based of the premade maze generator. This will hopefully give more flexibility

---
+++ Puzzle Duo VR/Assets/Scripts/Maze/Maze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze
+{
+    private Cell[,] cells;
+
+    private int rowsCount;
+    private int columnCount;
+
+    public Maze(int rowsCount, int columnCount)
+    {
+        this.rowsCount = rowsCount;
+        this.columnCount = columnCount;
+
+        cells = GenerateCells();
+    }
+
+    private Cell[,] GenerateCells()
+    {
+        Cell[,] cells = new Cell[rowsCount, columnCount];
+
+        return cells;
+    }
+
+    public bool IsInRange(int x, int y)
+    {
+        return x >= 0 && x < rowsCount && y >= 0 && y < columnCount;
+    }
+}
---
+++ Puzzle Duo VR/Assets/Scripts/Maze/MazeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [Header("Maze Dimensions")]
+    public int rows = 1;
+    public int columns = 1;
+
+    void Start()
+    {
+        Maze maze = new Maze(rows, columns);
+        maze.Generate();
+    }
+
+    void OnValidate()
+    {
+        rows = Mathf.Clamp(rows, 1, int.MaxValue);
+        columns = Mathf.Clamp(columns, 1, int.MaxValue);
+    }
+}
