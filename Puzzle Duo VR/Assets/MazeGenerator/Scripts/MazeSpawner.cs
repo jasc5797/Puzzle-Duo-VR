1c59ab7953d2acb779aced910d6dd1c8b43530c6
jasc5797 <jasc5797@yahoo.com>
2019-03-09 16:56:55 -0500

    Fixed bug with collectable interact script

--- Puzzle Duo VR/Assets/MazeGenerator/Scripts/MazeSpawner.cs
+++ Puzzle Duo VR/Assets/MazeGenerator/Scripts/MazeSpawner.cs
@@ -88,8 +88,8 @@ public class MazeSpawner : MonoBehaviour {
 				tmp = Instantiate(Floor,new Vector3(x,0,z), Quaternion.Euler(0,0,0)) as GameObject;
 				tmp.transform.parent = transform;
 
-                tmp = Instantiate(Floor, new Vector3(x, 4, z), Quaternion.Euler(0, 0, 0)) as GameObject;
-                tmp.transform.parent = transform;
+               // tmp = Instantiate(Floor, new Vector3(x, 4, z), Quaternion.Euler(0, 0, 0)) as GameObject;
+                //tmp.transform.parent = transform;
 
                 if (cell.WallRight){
 					tmp = Instantiate(Wall,new Vector3(x+CellWidth/2,0,z)+Wall.transform.position,Quaternion.Euler(0,90,0)) as GameObject;// right
--- Puzzle Duo VR/Assets/Scripts/CollectableInteract.cs
+++ Puzzle Duo VR/Assets/Scripts/CollectableInteract.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectable : MonoBehaviour
+public class CollectableInteract : MonoBehaviour
 {
 	private GameObject CollectableManagerObject;
 	private CollectableManager CollectableManagerScript;
