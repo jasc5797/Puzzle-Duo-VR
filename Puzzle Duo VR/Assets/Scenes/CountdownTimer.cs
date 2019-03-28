79ca9d8c6cc14068fa82bfaf008781854db4a344
Marcus275 <sanonm2@wit.edu>
2019-03-28 15:31:51 -0400

    I control time! Timer done.

---
+++ Puzzle Duo VR/Assets/Scenes/CountdownTimer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float timeRemaining;
+    float startingTime = 300;
+    float timeRemainingReset = 300;
+
+    [SerializeField] Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = startingTime ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+
+        int seconds = (int)(timeRemaining % 60);
+        int minutes = (int)(timeRemaining/ 60) % 60;
+        //int hours = (int)(timeRemaining / 3600) % 24;
+
+        string timerString = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        //countdownText.text = timeRemaining.ToString("00");
+        countdownText.text = timerString;
+
+        if (timeRemaining <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+            timeRemaining = timeRemainingReset;
+        }
+
+    }
+}
